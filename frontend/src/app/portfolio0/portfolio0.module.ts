95c2c7f53ade9b0b5003760923a030bca40ca071
Med Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2021-07-10 19:10:46 +0100

    started portfolio0 & landing page customizable animation

--- frontend/src/app/portfolio0/portfolio0.module.ts
+++ frontend/src/app/portfolio0/portfolio0.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { Portfolio0Component } from './portfolio0.component';
 import {Portfolio0RoutingModule} from "./portfolio0-routing.module";
 import { HeaderComponent } from './header/header.component';
+import { LandingSectionComponent } from './landing-section/landing-section.component';
 
 
 
 @NgModule({
   declarations: [
     Portfolio0Component,
-    HeaderComponent
+    HeaderComponent,
+    LandingSectionComponent
   ],
   imports: [
     CommonModule,
