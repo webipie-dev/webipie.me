fd38e21fe6a5f3d4a575878000a671d028220d3a
nour-karoui <nkaroui1998@gmail.com>
2021-07-16 21:33:09 +0100

    fix env variables

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  LINKEDIN_API_KEY: '77oj8s50xw1yt7',
+  LINKEDIN_SECRET: 'W8tanXzQrWJpjH6y',
+  LINKEDIN_REDIRECT_URL: 'http://localhost:4200/register/linkedin-verif',
+  LINKEDIN_SCOPE: "r_liteprofile%20r_emailaddress"
 };
