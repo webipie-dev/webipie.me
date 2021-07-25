89d06f908dcbe700e86aec303224934661422fc7
Med Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2021-07-25 09:05:37 +0100

    small changes

--- frontend/src/app/registration/header/header.component.ts
+++ frontend/src/app/registration/header/header.component.ts
@@ -16,8 +16,9 @@ export class HeaderComponent implements OnInit {
   constructor(private router : Router) { }
 
   ngOnInit(): void {
-    if(this.router.url === '/register/choose-template' || this.router.url === '/register/linkedin-verif'){
+    if(this.router.url === '/register/choose-template' || this.router.url.includes('/register/linkedin-verif')){
       this.scrolled=true;
+      console.log(this.router.url.indexOf('/register/linkedin-verif') );
     }
   }
   onMenuClick(event:any){
