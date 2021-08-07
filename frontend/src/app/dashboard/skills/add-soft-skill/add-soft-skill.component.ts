3452eaa873ee42c2a6a6509963643194d2dec9b5
Med Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2021-08-07 15:45:02 +0100

    fix skill page

--- frontend/src/app/dashboard/skills/add-soft-skill/add-soft-skill.component.ts
+++ frontend/src/app/dashboard/skills/add-soft-skill/add-soft-skill.component.ts
@@ -8,76 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class AddSoftSkillComponent implements OnInit {
   someValue = 5;
   selectedSkills = ['Karyn Wright'];
-    skills = [
-        {
-            'id': '5a15b13c36e7a7f00cf0d7cb',
-            'name': 'Front End'
-        },
-        {
-            'id': '5a15b13c2340978ec3d2c0ea',
-            'name': 'Back End'
-        },
-        {
-            'id': '5a15b13c663ea0af9ad0dae8',
-            'name': 'Mobile Development'
-        },
-        {
-            'id': '5a15b13cc9eeb36511d65acf',
-            'name': 'Rosales Russell'
-        },
-        {
-            'id': '5a15b13c728cd3f43cc0fe8a',
-            'name': 'Marquez Nolan'
-        },
-        {
-            'id': '5a15b13ca51b0aaf8a99c05a',
-            'name': 'Franklin James'
-        },
-        {
-            'id': '5a15b13cc3b9381ffcb1d6f7',
-            'name': 'Elsa Bradley'
-        },
-        {
-            'id': '5a15b13ce58cb6ff62c65164',
-            'name': 'Pearson Thompson'
-        },
-        {
-            'id': '5a15b13c90b95eb68010c86e',
-            'name': 'Ina Pugh'
-        },
-        {
-            'id': '5a15b13c2b1746e12788711f',
-            'name': 'Nguyen Elliott'
-        },
-        {
-            'id': '5a15b13c605403381eec5019',
-            'name': 'Mills Barnett'
-        },
-        {
-            'id': '5a15b13c67e2e6d1a3cd6ca5',
-            'name': 'Margaret Reynolds'
-        },
-        {
-            'id': '5a15b13c947c836d177aa85c',
-            'name': 'Yvette Navarro'
-        },
-        {
-            'id': '5a15b13c5dbbe61245c1fb73',
-            'name': 'Elisa Guzman'
-        },
-        {
-            'id': '5a15b13c38fd49fefea8db80',
-            'name': 'Jodie Bowman'
-        },
-        {
-            'id': '5a15b13c9680913c470eb8fd',
-            'name': 'Diann Booker'
-        }
-    ];
+    skills :any;
   constructor() { }
 
   ngOnInit(): void {
-
+    this.skills = [
+        'Karyn Wright',
+        'Rochelle Estes',
+        'Mendoza Ruiz',
+        'Rosales Russell',
+        'Marquez Nolan',
+        'Franklin James',
+        'Elsa Bradley',
+        'Pearson Thompson',
+        'Ina Pugh',
+        'Nguyen Elliott',
+        'Mills Barnett',
+        'Margaret Reynolds',
+        'Yvette Navarro',
+        'Elisa Guzman',
+        'Jodie Bowman',
+        'Diann Booker'
+    ];
   }
 
 }
