fc5f54fdb06c93658b00b6df02d955a2ecfddfb8
Med Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2021-08-08 16:22:16 +0100

    finish contact & education design section

--- frontend/src/app/dashboard/design/contact-section/contact-section.component.ts
+++ frontend/src/app/dashboard/design/contact-section/contact-section.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-section.component.scss']
 })
 export class ContactSectionComponent implements OnInit {
-
+  textAlignment = [false,true];
+  someValue = 5;
   constructor() { }
-
+  onItemChange(softSkill:any){
+    this.change(softSkill.target.value);
+  }
+  change(s:string){
+    switch (s){
+      case 'first':{
+        this.textAlignment[0]=true;
+        this.textAlignment[1]=false;
+        break;
+      }
+      case 'second':{
+        this.textAlignment[1]=true;
+        this.textAlignment[0]=false;
+        break;
+      }
+    }
+  }
+  select(i:number){
+    let s:string;
+    if(i==1){
+      s="first";
+    }else{
+      s="second";
+    }
+    this.change(s);
+  }
   ngOnInit(): void {
   }
-
 }
