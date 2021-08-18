048ab7fa54f83edd893ea17b36533c2bf12897aa
Med Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2021-08-18 10:10:31 +0100

    fixed portfolio header

--- frontend/src/app/portfolio0/header/header.component.ts
+++ frontend/src/app/portfolio0/header/header.component.ts
@@ -7,14 +7,19 @@ import { Component, HostListener, OnInit, Output, EventEmitter } from '@angular/
 })
 export class HeaderComponent implements OnInit {
   scrolled = false;
+  name : string = "John Doe"
   @Output() aboutmeClick = new EventEmitter<any>();
   @Output() expertiseClick = new EventEmitter<any>();
   @Output() experienceClick = new EventEmitter<any>();
   @Output() testimonialsClick = new EventEmitter<any>();
+  @Output() educationClick = new EventEmitter<any>();
+  @Output() achievementsClick = new EventEmitter<any>();
+  @Output() portfolioClick = new EventEmitter<any>();
   @Output() contactClick = new EventEmitter<any>();
   constructor() { }
 
   ngOnInit(): void {
+    this.name = JSON.parse(localStorage.getItem('portfolio')!).name;
   }
   clickContact(event:any){
     this.contactClick.emit(event);
@@ -30,6 +35,15 @@ export class HeaderComponent implements OnInit {
   }
   clickTestimonials(event:any){
     this.testimonialsClick.emit(event);
+  }
+  clickEducation(event:any){
+    this.educationClick.emit(event);
+  }
+  clickAchievements(event:any){
+    this.achievementsClick.emit(event);
+  }
+  clickPortfolio(event:any){
+    this.portfolioClick.emit(event);
   }
   @HostListener('window:scroll', ['$event']) 
     doSomething(event:any) {
