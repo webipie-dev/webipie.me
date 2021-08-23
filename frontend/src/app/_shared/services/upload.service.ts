3cb96eb83472816e71eb8f72e9d0a6299e0d718b
butterjack <jawher_bouhouch@yahoo.com>
2021-08-23 20:45:26 +0100

    add upload

---
+++ frontend/src/app/_shared/services/upload.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UtilsUrl } from '../utils/utils-url';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService{
+
+  url = UtilsUrl.url;
+  suffix = '/upload';
+  constructor(protected http: HttpClient) {}
+
+  cvUpload(body: any): Promise<any>{
+    return this.http.post(this.url + this.suffix + '/cv', body).toPromise();
+  }
+
+  imageUpload(body: any): Promise<any>{
+    return this.http.post(this.url + this.suffix + '/image', body).toPromise();
+  }
+
+  videoUpload(body: any): Promise<any>{
+    return this.http.post(this.url + this.suffix + '/video', body).toPromise();
+  }
+}
