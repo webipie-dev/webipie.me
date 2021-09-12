af70157e1f465fd849d597d27b28b5ca1b168050
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-09-12 13:53:31 +0100

    fix preview buttons

--- frontend/src/app/dashboard/header/header-menu/header-menu.component.ts
+++ frontend/src/app/dashboard/header/header-menu/header-menu.component.ts
@@ -1,23 +1,36 @@
 import {Component, OnInit} from '@angular/core';
 import {environment} from "../../../../environments/environment";
 import {ActivatedRoute, Router} from "@angular/router";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header-menu',
   templateUrl: './header-menu.component.html'
 })
 export class HeaderMenuComponent implements OnInit {
+  url?: string
 
 
   constructor(private router: Router, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.url = JSON.parse(localStorage.getItem('portfolio')!).url;
+    if(!this.url){
+      Swal.fire({
+        title: 'Error!',
+        text: 'something went wrong, please refresh',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      });
+    }
   }
 
   openSite() {
-    const url = JSON.parse(localStorage.getItem('portfolio')!).url + environment.PORT;
-    window.open('//' + 'google.com', '_blank');
+    if(this.url){
+      let url = `${environment.protocol}://${this.url}${environment.PORT}`;
+      window.open(url, '_blank');
+    }
   }
 
   changeTemplate() {
