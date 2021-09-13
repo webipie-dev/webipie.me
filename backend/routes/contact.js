9581e8977e70a18fe05019f4ec9b0f9d643df43c
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-09-13 22:24:53 +0100

    add contact support endpoint

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,9 +1,10 @@
-const {contactUser} = require ('../services/contact')
+const {contactUser, contactSupport} = require ('../services/contact')
 const express = require('express');
 
 const router = express.Router();
 
 
 router.post("", contactUser);
+router.post("/support", contactSupport);
 
 module.exports = router;
