be3214dddcf8f004351d70d57dbd2b5ef2422149
nour-karoui <nkaroui1998@gmail.com>
2021-09-15 13:48:43 +0100

    index page done

--- frontend/src/app/index/frontpage/frontpage.component.ts
+++ frontend/src/app/index/frontpage/frontpage.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
 import { faMouse } from '@fortawesome/free-solid-svg-icons';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-frontpage',
@@ -9,12 +10,21 @@ import { faMouse } from '@fortawesome/free-solid-svg-icons';
 export class FrontpageComponent implements OnInit {
   mouse = faMouse;
   class = '';
-  constructor(private renderer:Renderer2) { }
+  constructor(private renderer:Renderer2, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
   onIntersection(){
     this.class = 'active';
   }
-  
+
+  getStarted() {
+    /**
+     * if logged in and has portfolio, redirect to dashboard
+     * if logged in and no portfolio, redirect to choose template
+     * if not logged in, redirect to signup
+     */
+    const navigateTo = localStorage.getItem('token') ? (localStorage.getItem('portfolio') ? '/dashboard' : '/templates/choose-template') : '/register/signup'
+    this.router.navigate([navigateTo]);
+  }
 }
