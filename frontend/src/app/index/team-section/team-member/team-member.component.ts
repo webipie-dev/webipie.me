4f80a8f3fe51515cdbad849719352d7d3fcce1c6
Med Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2021-09-21 13:54:28 +0100

    made team section's content modifiable

--- frontend/src/app/index/team-section/team-member/team-member.component.ts
+++ frontend/src/app/index/team-section/team-member/team-member.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
@@ -10,6 +10,12 @@ export class TeamMemberComponent implements OnInit {
   overlay = false;
   linkedin = faLinkedin;
   github = faGithub;
+  @Input() name = "John Doe";
+  @Input() position = "Web Developer";
+  @Input() picture = "assets/johndoe.jpg";
+  @Input() githublink = "#";
+  @Input() linkedinlink = "#";
+  @Input() description = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum fuga repellendus reprehenderit accusamus voluptatem, natus, doloribus quaerat quo molestias."
   constructor() { }
 
   ngOnInit(): void {
