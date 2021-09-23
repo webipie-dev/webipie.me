9c109916227a0fffb21bd2c8f8cf2ee8a23bbda6
butterjack <jawher_bouhouch@yahoo.com>
2021-09-23 15:26:00 +0100

    fix etl

--- etl/hard_skills/main.py
+++ etl/hard_skills/main.py
@@ -1,4 +1,3 @@
-import wptools
 import requests
 from bs4 import BeautifulSoup
 import pymongo
@@ -33,48 +32,43 @@ def find_skill(db, skill):
 
 
 def search_logo(keyword):
-    try:
-        page = wptools.page(keyword, lang="fr")
-        page.get_parse()
-        if 'image' in page.data:
-            image = page.data['image'][0]['url']
-            if "logo" in image or ("icon" in image and keyword in image):
-                return image
-    except:
-        pass
 
-    try:
-        page = wptools.page(keyword, lang="eng")
-        page.get_parse()
-        if 'image' in page.data:
-            image = page.data['image'][0]['url']
-            if "logo" in image or ("icon" in image and keyword in image):
-                return image
-    except:
-        pass
+    url = "https://worldvectorlogo.com/fr/chercher/" + keyword
 
-    try:
-        response = requests.get(
-            url="https://en.wikipedia.org/wiki/" + keyword.replace(' ', '_'),
-        )
-        soup = BeautifulSoup(response.content, 'html.parser')
+    headers = {
+        'authority': "worldvectorlogo.com",
+        'cache-control': "no-cache",
+        'upgrade-insecure-requests': "1",
+        'user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+        'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        'sec-fetch-site': "same-origin",
+        'sec-fetch-mode': "navigate",
+        'sec-fetch-user': "?1",
+        'sec-fetch-dest': "document",
+        'sec-ch-ua': "\"Google Chrome\";v=\"93\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"93\"",
+        'sec-ch-ua-mobile': "?0",
+        'sec-ch-ua-platform': "\"Linux\""
+        }
 
-        images = soup.select("img")
-        logos = [image['src'] for image in images if "logo" in image['src']]
-        if logos[0] != None:
-            return logos[0]
-    except:
-        pass
+    response = requests.request("GET", url, headers=headers)
+
+    #print(response.text)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    return soup.select(".logo__img")[0]['src']
         
 
 
 def upload_to_aws(url, s3_file):
-    response = urlopen('https:' + url)
+    response = urlopen(url)
 
     s3 = boto3.client('s3', aws_access_key_id=config.ACCESS_KEY,
                       aws_secret_access_key=config.SECRET_KEY)
+
+    key = "skills/" + s3_file + "png"
     try:
-        s3.upload_fileobj(response, config.S3_BUCKET, "skills/" + s3_file + ".png")
+        s3.upload_fileobj(response, config.S3_BUCKET, key,
+                          ExtraArgs={'ACL': 'public-read'})
         print("Upload Successful")
         return True
     except  Exception as e:
@@ -96,7 +90,7 @@ for skill in skills:
             icon = "https://s3-{0}.amazonaws.com/{1}/{2}".format(
                             config.AWS_REGION,
                             config.S3_BUCKET,
-                            "skills/" + skill + ".png")
+                            "skills/" + skill + ".svg")
             insert_skill(db, skill, icon)
         else:
             insert_skill(db, skill)
