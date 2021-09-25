fbf9bd08d3368a6d9696dacfc58707b5bd952a94
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-09-25 21:59:27 +0100

    fix resend email

--- backend/services/user.js
+++ backend/services/user.js
@@ -170,7 +170,7 @@ module.exports = {
             <div class="cos-body" style="text-align:center">
                 <!-- <img src="https://webipie.me/assets/SVG/email.svg"> -->
                 <img src="https://webipie.me/assets/email.png" style="width:18vw; height:18vw; object-fit:cover ;background-color: white; border-radius:50%">
-                <h1 style="width:90%">Hello ${name}, Welcome to Webipie.me</h1>
+                <h1 style="width:90%">Hello, Welcome to Webipie.me</h1>
                 <p style="font-size:20px; width:90%">We Created a personal account for you. Please confirm your e-mail adress</p>
                 <a href="${httpProtocol}://${clientHostname}${portString}/register/confirmation?token=${token}" style="text-decoration: none; color: white; background-color: #3aaf85; padding:15px 25px; margin-bottom:50px">Confirm Email</a>
                 <hr style="margin-top:25px">
