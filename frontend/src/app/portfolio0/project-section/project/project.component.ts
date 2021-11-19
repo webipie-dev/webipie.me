13d69221352f879404b874216f71c45b65a595f5
butterjack <jawher_bouhouch@yahoo.com>
2021-11-19 20:38:27 +0100

    add name on hover project & fix on loading hard skill

--- frontend/src/app/portfolio0/project-section/project/project.component.ts
+++ frontend/src/app/portfolio0/project-section/project/project.component.ts
@@ -96,4 +96,15 @@ export class ProjectComponent implements OnInit,OnChanges,AfterViewInit {
   exit(el: HTMLElement){
     el.setAttribute('class','overlay')
   }
+  hexToRGB(hex: any, alpha: any) {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    } else {
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+  }
 }
