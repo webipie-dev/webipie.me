70e8266655f12eab126dd695667a6f0eabab050f
dalideco <dalissj1@gmail.com>
2022-01-17 17:18:18 +0100

    hard skills

--- frontend/src/app/portfolio1/expertise-section/expertise-section.component.ts
+++ frontend/src/app/portfolio1/expertise-section/expertise-section.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SoftSkillModel } from 'src/app/_shared/models/soft-skill.model';
+import { TechnicalSkillDeveloperModel } from 'src/app/_shared/models/technical-skill-developer';
 
 @Component({
   selector: 'app-expertise-section',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpertiseSectionComponent implements OnInit {
 
+  softSkills?: [SoftSkillModel];
+  hardSkills?: [TechnicalSkillDeveloperModel] | [any];
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.hardSkills = JSON.parse(localStorage.getItem('portfolio')!).technicalSkills;
+    this.softSkills = JSON.parse(localStorage.getItem('portfolio')!).softSkills;
+    console.log(this.hardSkills)
   }
 
 }
