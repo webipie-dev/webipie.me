b5603926436354e7beb9df8eee964f742850dbd5
dalideco <dalissj1@gmail.com>
2022-01-18 18:36:11 +0100

    fetching career

--- frontend/src/app/portfolio1/experience-section/experience-section.component.ts
+++ frontend/src/app/portfolio1/experience-section/experience-section.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AchievementModel } from 'src/app/_shared/models/achievement.model';
+import { EducationModel } from 'src/app/_shared/models/education.model';
+import { VolunteeringExperienceModel } from 'src/app/_shared/models/volunteering-experience.model';
+import { WorkExperienceModel } from 'src/app/_shared/models/work-experience.model';
 
 @Component({
   selector: 'app-experience-section',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceSectionComponent implements OnInit {
 
+  workExperiences?: WorkExperienceModel[];
+  volunteeringExperiences?: VolunteeringExperienceModel[];
+  education?: [EducationModel];
+  achievements?: [AchievementModel];
   constructor() { }
 
   ngOnInit(): void {
+    this.workExperiences = JSON.parse(localStorage.getItem('portfolio')!).workExperiences;
+    this.volunteeringExperiences = JSON.parse(localStorage.getItem('portfolio')!).volunteeringExperiences;
+    this.education = JSON.parse(localStorage.getItem('portfolio')!).education
+    this.achievements = JSON.parse(localStorage.getItem('portfolio')!).achievements;
+    console.log(this.education, this.achievements)
   }
-
 }
